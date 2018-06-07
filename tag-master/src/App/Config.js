17663cc1224d1f68de99834ae2dd066b850a76ef
Tomasz Przybył <tomasz.przybyl.it@gmail.com>
2018-06-07 17:11:29 +0200

    Update Config.js

--- tag-master/src/App/Config.js
+++ tag-master/src/App/Config.js
@@ -2,14 +2,14 @@ let config;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   // development
   config = {
-    app_client_id: 'c4603a8a3489cb2291a8d3825c4c43e6',
+    app_client_id: 'your_client_id',
     api_url: "https://accounts.livechatinc.com/",
     serverUrl: 'http://localhost:4000',
   }
 } else {
   // production
   config = {
-    app_client_id: 'c4603a8a3489cb2291a8d3825c4c43e6',
+    app_client_id: 'your_client_id',
     api_url: "https://accounts.livechatinc.com/",
     serverUrl: 'https://us-central1-livechat-experiments.cloudfunctions.net/restApi',
   }
