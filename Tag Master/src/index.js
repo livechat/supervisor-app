aa5b8acf4b3ac4f250730ed94d5b48b18f58a602
Dominik Slawkowski <dominik.slawkowski@gmail.com>
2019-05-29 15:36:59 +0200

    create useLiveChat hook

--- Tag Master/src/index.js
+++ Tag Master/src/index.js
@@ -15,11 +15,11 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const AppWithAuth = () => {
+const useLiveChat = ({ client_id, account_url }) => {
   const [accessToken, setAccessToken] = useState(null);
   LiveChat.init({ authorize: false });
+
   useEffect(() => {
-    const { client_id, account_url } = config;
     accountsSdk.init({
       client_id,
       onIdentityFetched: (error, data) => {
@@ -33,6 +33,12 @@ const AppWithAuth = () => {
       }
     });
   });
+
+  return [accessToken];
+};
+
+const AppWithAuth = () => {
+  const [accessToken] = useLiveChat(config);
 
   if (!accessToken) {
     return <Spinner marginTop="calc(100% - 50px)" />;
--- progress/src/index.js
+++ progress/src/index.js
@@ -22,11 +22,11 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const AppWithAuth = () => {
+const useLiveChat = ({ client_id, account_url }) => {
   const [accessToken, setAccessToken] = useState(null);
   LiveChat.init({ authorize: false });
+
   useEffect(() => {
-    const { client_id, account_url } = config;
     accountsSdk.init({
       client_id,
       onIdentityFetched: (error, data) => {
@@ -40,6 +40,12 @@ const AppWithAuth = () => {
       }
     });
   });
+
+  return [accessToken];
+};
+
+const AppWithAuth = () => {
+  const [accessToken] = useLiveChat(config);
 
   if (!accessToken) {
     return <Spinner marginTop="calc(100% - 50px)" />;
