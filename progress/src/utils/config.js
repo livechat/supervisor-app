506237fb78f6d485025629eb6470f385d73f625f
kamilatokarska <102953526+kamilatokarska@users.noreply.github.com>
2023-03-16 10:11:32 +0100

    Delete Progress directory DED-520

--- progress/src/utils/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  client_id: "<YOUR_CLIENT_ID>",
-  server_url:
-    "https://us-central1-livechat-experiments.cloudfunctions.net/restApi",
-  account_url: "https://accounts.livechatinc.com/"
-};
-
-export default config;
